f97860dc37b246c853938cd86882c2a157e3090b
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2020-05-05 12:17:39 +0200

    Revert "Switch to Flask-Cache"
    This reverts commit b0f30e08ebd08504879481dcd1fda786a0312146.

--- setup.py
+++ setup.py
@@ -7,5 +7,5 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
-    install_requires=['Flask', 'Flask-Cache', 'flask-cors', 'requests']
+    install_requires=['Flask', 'Flask-Caching', 'flask-cors', 'requests']
 )
